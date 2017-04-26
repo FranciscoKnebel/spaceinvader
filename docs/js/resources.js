a809b282388d50f9364679183a4ffb574a56b486
Francisco Knebel <franciscopaivaknebel@gmail.com>
2017-04-26 18:51:58 -0300

    Added docs folder for github.io

---
+++ docs/js/resources.js
@@ -0,0 +1,37 @@
+game.resources = [
+    {
+        "name": "tronicles",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "fire",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "hit",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "lost",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "won",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "player",
+        "type": "image",
+        "src": "data/img/player.png"
+    },
+    {
+        "name": "ships",
+        "type": "image",
+        "src": "data/img/ships.png"
+    }
+];
